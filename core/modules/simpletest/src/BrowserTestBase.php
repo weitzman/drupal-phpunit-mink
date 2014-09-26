dcfda17c9b6694f25e53572ae37839b98e03649c
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-09-26 18:21:02 +1000

    Forward XDEBUG cookie if developing is listening

--- core/modules/simpletest/src/BrowserTestBase.php
+++ core/modules/simpletest/src/BrowserTestBase.php
@@ -52,6 +52,32 @@ abstract class BrowserTestBase extends RunnerTestBase {
     $this->mink = new Mink();
     $this->mink->registerSession('goutte', $session);
     $this->mink->setDefaultSessionName('goutte');
+
+    // In order to debug web tests you need to either set a cookie, have the
+    // Xdebug session in the URL or set an environment variable in case of CLI
+    // requests. If the developer listens to connection when running tests, by
+    // default the cookie is not forwarded to the client side, so you cannot
+    // debug the code running on the test site. In order to make debuggers work
+    // this bit of information is forwarded. Make sure that the debugger listens
+    // to at least three external connections.
+    $request = \Drupal::request();
+    $cookie_params = $request->cookies;
+    if ($cookie_params->has('XDEBUG_SESSION')) {
+      $session->setCookie('XDEBUG_SESSION', $cookie_params->get('XDEBUG_SESSION'));
+    }
+    // For CLI requests, the information is stored in $_SERVER.
+    $server = $request->server;
+    if ($server->has('XDEBUG_CONFIG')) {
+      // $_SERVER['XDEBUG_CONFIG'] has the form "key1=value1 key2=value2 ...".
+      $pairs = explode(' ', $server->get('XDEBUG_CONFIG'));
+      foreach ($pairs as $pair) {
+        list($key, $value) = explode('=', $pair);
+        // Account for key-value pairs being separated by multiple spaces.
+        if (trim($key, ' ') == 'idekey') {
+          $session->setCookie('XDEBUG_SESSION', trim($value, ' '));
+        }
+      }
+    }
   }
 
   /**
